8487b5cb97ee7a855710c18efd8c511c9e7e91d7
Федулов Филипп Юрьевич <62757457+FedulovPhilipp@users.noreply.github.com>
2022-07-28 18:06:04 +0500

    первый рабочий вариант

--- pricing/form1/forms.py
+++ pricing/form1/forms.py
@@ -3,6 +3,25 @@ from django import forms
 
 class PriceCalc(forms.Form):
     route_date = forms.DateField(help_text="Дата поездки")
-    bus_class = forms.CharField(help_text="Класс ТС")
-    distance = forms.IntegerField(help_text="Расстояние до конечной точки")
-    rate = forms.IntegerField(help_text="Тариф")
+    description = forms.CharField(help_text="Заказ")
+    distance = forms.IntegerField(help_text="Расстояние до конечной точки в км", required=False, initial=0)
+    duration = forms.IntegerField(help_text="Продолжительность работы в часах", required=False, initial=0)
+    delivery = forms.IntegerField(help_text='Подача к месту заказа в часах', required=False, initial=0)
+
+    vehicle_class = [('0', 'Микроавтобус эконом 17-18 мест'),
+                     ('1', 'Микроавтобус комфорт 19-20 мест'),
+                     ('2', 'Микроавтобус представительский 19-20 мест'),
+                     ('3', 'Аврора 29-35 мест'),
+                     ('4', 'Ютонг или Хайгер 35-40 мест'),
+                     ('5', 'Ютонг или Хайгер 41-45 мест'),
+                     ('6', 'Ютонг или Хайгер 47 - 51 мест'),
+                     ('7', 'Хайгер 55 мест'),
+                     ]
+
+    bus_class = forms.ChoiceField(choices=vehicle_class, help_text="Класс ТС")
+    accomodation = forms.IntegerField(help_text="Проживание водителей в рублях", required=False, initial=0)
+    drivers_fee = forms.IntegerField(help_text="Командировочные водителей в рублях", required=False, initial=0)
+    extra = forms.IntegerField(help_text="Наценка в процентах (если скидка, со знаком -)", required=False, initial=0)
+
+
+
--- pricing/form1/views.py
+++ pricing/form1/views.py
@@ -4,12 +4,34 @@ from .forms import PriceCalc
 
 def show_form(request):
     if request.method == "POST":
+        rates = [('0', 1, 'Микроавтобус эконом 17-18 мест'),
+                 ('1', 1.08, 'Микроавтобус комфорт 19-20 мест'),
+                 ('2', 1.2, 'Микроавтобус представительский 19-20 мест'),
+                 ('3', 1.3, 'Аврора 29-35 мест'),
+                 ('4', 1.4, 'Ютонг или Хайгер 35-40 мест'),
+                 ('5', 1.6, 'Ютонг или Хайгер 41-45 мест'),
+                 ('6', 1.7, 'Ютонг или Хайгер 47 - 51 мест'),
+                 ('7', 2.,  'Хайгер 55 мест'),
+                 ]
+        base_rate = [1200, 30]
         route_date = request.POST.get('route_date')
-        bus_class = request.POST.get('bus_class')
-        distance = request.POST.get('distance')
-        rate = request.POST.get('rate')
-        price = int(rate)*int(distance)*2
-        data = {"route_date": route_date, 'bus_class': bus_class, 'distance': distance, 'rate': rate, 'price': price}
+        bus_class = int(request.POST.get('bus_class'))
+        distance = int(request.POST.get('distance'))
+        duration = int(request.POST.get('duration'))
+        delivery = int(request.POST.get('delivery'))
+        accomodation = int(request.POST.get('accomodation'))
+        drivers_fee = int(request.POST.get('drivers_fee'))
+        extra = int(request.POST.get('extra'))
+        vihicle_class = rates[bus_class][2]
+        milage = distance * 2
+        milage_cost = base_rate[1] * milage * rates[bus_class][1]
+        duration_cost = base_rate[0] * duration * rates[bus_class][1]
+        delivery_cost = base_rate[0] * delivery * rates[bus_class][1]
+        full_cost = int((milage_cost + duration_cost + delivery_cost + accomodation + drivers_fee) * (1 + extra/100))
+
+        data = {"route_date": route_date, 'drivers_fee': drivers_fee, 'delivery_cost': delivery_cost,
+                'duration_cost': duration_cost, 'extra': extra, 'vihicle_class': vihicle_class, 'duration': duration,
+                'milage_cost': milage_cost, 'accomodation': accomodation,  'milage': milage, 'full_cost': full_cost}
         return render(request, 'form2.html', context=data)
     form = PriceCalc()
     return render(request, 'form1.html', {'form': form})
